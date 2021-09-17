2cffad68f6405151ac351e57c800cf6ab9ea0fc4
pseudoname <90123698+pseudoname23@users.noreply.github.com>
2021-09-17 09:08:31 -0500

    More physics starter work
    Man, I wish I had more free time.
    Turned the player into a class and added a little bit more physics. Nothing is doing anything yet.

--- tk_test.py
+++ tk_test.py
@@ -5,18 +5,28 @@ xV, yV = 0, 0
 x, y = 0, 0
 
 
-def velocityUpdate():
-    if yV > 0:
-        pass
+def updateP():
+    global x, y, xV, yV
+    x += xV
+    y += yV
+
+
+class Player:
+    def __init__(self, parent, xPos, yPos, color):
+        self.parent, self.x, self.y, self.color = parent, xPos, yPos, color
+
+    def create(self):
+        self.parent.create_rectangle(self.x+950, self.y+540, self.x+970, self.y+520, fill=self.color, tags='player')
 
 
 class Rectangle:
     def __init__(self, parent, xPos, yPos, xPos2, yPos2):
-        self.parent,self.xPos,self.yPos,self.xPos2,self.ypos2 = parent,xPos+960,-yPos+540,xPos2+960,-yPos2+540
-        # Will be used for platforming soon
+        self.parent,self.xPos,self.yPos,self.xPos2,self.yPos2 = parent,xPos+960,-yPos+540,xPos2+960,-yPos2+540
+        self.xBounds = self.xPos, self.xPos2
+        self.yBounds = self.yPos, self.yPos2
 
     def create(self):
-        self.parent.create_rectangle(self.xPos, self.yPos, self.xPos2, self.ypos2,
+        self.parent.create_rectangle(self.xPos, self.yPos, self.xPos2, self.yPos2,
                                      fill='#FFF000', outline='blue', tags=('rectangle', 'platform'))
 
 
@@ -38,5 +48,6 @@ canvas.create_line(960, -20 + 540, 960, 20 + 540, width=5, tags='center')
 rect = Rectangle(canvas, -100, -50, 100, 0)
 rect.create()
 
-canvas.create_rectangle(-10+960, 540, 10+960, -20+540, fill='#10fa7c', tags='player')
+plr = Player(canvas, 0, 0, '#000000')
+plr.create()
 root.mainloop()
