c3b9b388ec3d8f60611f844558d11c46171d07d3
pseudoname <90123698+pseudoname23@users.noreply.github.com>
2021-09-22 19:46:03 -0500

    Update tk_test.py
    misc

--- tk_test.py
+++ tk_test.py
@@ -6,17 +6,6 @@ TPS = 60
 tCount = 0
 
 
-class Player:
-    def __init__(self, parent, xPos, yPos, color):
-        self.parent, self.x, self.y, self.color = parent, xPos, yPos, color
-
-    def create(self):
-        self.parent.create_rectangle(self.x+950, self.y+540, self.x+970, self.y+520, fill=self.color, tags='player')
-        
-    def bind(self, btn, cmd):
-        self.parent.tag_bind(4, btn, cmd)
-
-
 class Rectangle:
     def __init__(self, parent, xPos, yPos, xPos2, yPos2):
         self.parent,self.xPos,self.yPos,self.xPos2,self.yPos2 = parent,xPos+960,-yPos+540,xPos2+960,-yPos2+540
@@ -33,7 +22,7 @@ class Canv(Canvas):
         super().__init__(parent, **kwargs)
 
 
-root = Tk() 
+root = Tk()
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
@@ -46,8 +35,11 @@ canvas.create_line(960, -20 + 540, 960, 20 + 540, width=5, tags='center') # 2
 rect = Rectangle(canvas, -100, -50, 100, 0) # 3
 rect.create()
 
-plr = Player(canvas, 0, 0, '#000000') # 4
-plr.create()
+plr = canvas.create_rectangle(950, 540, 970, 520, fill='#000000', tags='player') # 4
+
+def groove(self):
+    canvas.move(4, 10, 0)
+canvas.bind('<ButtonPress-1>', groove)
 root.mainloop()
 
 #while y >= 0:
