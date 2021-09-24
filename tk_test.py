bef8a2222ad636adb0e9defbdd956170504573d2
pseudoname <90123698+pseudoname23@users.noreply.github.com>
2021-09-24 08:56:35 -0500

    PHYSICS!!!!
    Fucking FINALLY. Click to jump. Took me long enough.

--- tk_test.py
+++ tk_test.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 import time
-x,y,xV,yV=0,0,0,0
+x,y,xV,yV = 0,0,0,10
 TPS = 60
 tCount = 0
 
@@ -26,27 +26,40 @@ root = Tk()
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-canvas = Canv(root, width=1920, height=1080) # 0
+canvas = Canv(root, width=1920, height=1080)  # 0
 canvas.grid(column=0, row=0, sticky=(N, W, E, S))
 
-canvas.create_line(-20 + 960, 540, 20 + 960, 540, width=5, tags='center') # 1
-canvas.create_line(960, -20 + 540, 960, 20 + 540, width=5, tags='center') # 2
+canvas.create_line(-20 + 960, 540, 20 + 960, 540, width=5, tags='center')  # 1
+canvas.create_line(960, -20 + 540, 960, 20 + 540, width=5, tags='center')  # 2
 
-rect = Rectangle(canvas, -100, -50, 100, 0) # 3
+rect = Rectangle(canvas, -100, -50, 100, 0)  # 3
 rect.create()
 
-plr = canvas.create_rectangle(950, 540, 970, 520, fill='#000000', tags='player') # 4
+plr = canvas.create_rectangle(950, 540, 970, 520, fill='#000000', tags='player')  # 4
 
-def groove(self):
-    canvas.move(4, 10, 0)
-canvas.bind('<ButtonPress-1>', groove)
+
+def tick():
+    global y, yV, tCount, TPS
+    if y >= 0:
+        y += yV
+        yV -= 64/TPS
+        tCount += 1
+        time.sleep(1/TPS)
+    else:
+        y = 0
+        yV = 0
+
+
+def jump(n):  # For some reason the code breaks if i don't include an argument
+    global yV, y
+    while True:
+        canvas.move(4, 0, -yV)
+        tick()
+        Tk.update(root)
+        if y < 5:
+            y = 10
+            break
+
+
+canvas.bind('<ButtonPress-1>', jump)
 root.mainloop()
-
-#while y >= 0:
-#    y += yV
-#    yV -= 64/TPS
-#    tCount += 1
-#    time.sleep(1/TPS)
-#y = 0
-#yV = 0
-# saving this code for later
