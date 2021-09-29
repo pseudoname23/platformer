26b07db4e3a0aad9ee65cb2b27b2b70e1711829f
pseudoname <90123698+pseudoname23@users.noreply.github.com>
2021-09-29 09:00:39 -0500

    Update (HALF DONE)
    Unfinished and non-functional

--- main.py
+++ main.py
@@ -5,28 +5,27 @@ x,y,xV,yV = 0,0,0,10
 TPS = 60
 tCount = 0
 
+objBounds = {}
+
 
 class Rectangle:
     def __init__(self, parent, xPos, yPos, xPos2, yPos2):
+        global objBounds
         self.parent,self.xPos,self.yPos,self.xPos2,self.yPos2 = parent,xPos+960,-yPos+540,xPos2+960,-yPos2+540
-        self.xBounds = self.xPos, self.xPos2
-        self.yBounds = self.yPos, self.yPos2
+        self.bounds = self.xPos, self.xPos2
+        self.id = -1
 
     def create(self):
-        self.parent.create_rectangle(self.xPos, self.yPos, self.xPos2, self.yPos2,
-                                     fill='#FFF000', outline='blue', tags=('rectangle', 'platform'))
-
-
-class Canv(Canvas):
-    def __init__(self, parent, **kwargs):
-        super().__init__(parent, **kwargs)
+        self.id = self.parent.create_rectangle(self.xPos, self.yPos, self.xPos2, self.yPos2,
+                                               fill='#FFF000', outline='blue', tags=('rectangle', 'platform'))
+        objBounds[self.id] = self.parent.coords(self.id)
 
 
 root = Tk()
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-canvas = Canv(root, width=1920, height=1080)  # 0
+canvas = Canvas(root, width=1920, height=1080)  # 0
 canvas.grid(column=0, row=0, sticky=(N, W, E, S))
 
 canvas.create_line(-20 + 960, 540, 20 + 960, 540, width=5, tags='center')  # 1
@@ -38,28 +37,34 @@ rect.create()
 plr = canvas.create_rectangle(950, 540, 970, 520, fill='#000000', tags='player')  # 4
 
 
-def tick():
+def gravitate():
     global y, yV, tCount, TPS
     if y >= 0:
         y += yV
         yV -= 64/TPS
-        tCount += 1
-        time.sleep(1/TPS)
     else:
-        y = 0
         yV = 0
+
+def detectCollision(bounds, player):
+    coords = canvas.coords(player)
+    for v in bounds.values():
+        
+        
+
 
 
 def jump(n):  # For some reason the code breaks if i don't include an argument
     global yV, y
     while True:
         canvas.move(4, 0, -yV)
-        tick()
+        gravitate()
         Tk.update(root)
         if y < 5:
             y = 10
+            Tk.update(root)
             break
 
 
 canvas.bind('<ButtonPress-1>', jump)
-root.mainloop()
+print(objBounds)
+print(canvas.coords(4))
